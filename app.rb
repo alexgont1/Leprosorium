54cb1e9e07022c96dcc70e872a617b471cadef42
alexgont1 <alexgont1@gmail.com>
2018-12-29 19:16:52 -0600

    added table Posts

--- app.rb
+++ app.rb
@@ -13,44 +13,17 @@ before do
 end
 
 configure do
-  enable :sessions
-end
-
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Hello stranger'
-  end
-end
-
-before '/secure/*' do
-  unless session[:identity]
-    session[:previous_url] = request.path
-    @error = 'Sorry, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
-  end
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts 
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_date DATE,
+    content TEXT
+  )'
 end
 
 get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
-end
-
-get '/login/form' do
-  erb :login_form
-end
-
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
-end
-
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+  erb "Hello! It's your new web site"
 end
 
 get '/new' do
